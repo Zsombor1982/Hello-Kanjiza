ea9a377c531d654ad84948bb4dfa5d5ce9d563bd
Zsombor1982 <46742071+Zsombor1982@users.noreply.github.com>
2019-01-16 10:03:02 +0100

    Add files via upload

---
+++ 1.2.c
@@ -0,0 +1,11 @@
+/* Ilustruje \n \t \\ \" i %% */
+#include <stdio.h>
+
+main()
+{
+	printf("\"Zdravo, svima\"\n");
+	printf("\\n\tprelazak u novi red\n");
+	printf("\\t\ttabulator\n");
+	printf("\\\\\tkosa crta\n");
+	printf("%%%%\tprocenat\n");
+}
