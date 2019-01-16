3063ebf5a20f290b4b6b7141010a94feac158b42
Zsombor1982 <46742071+Zsombor1982@users.noreply.github.com>
2019-01-16 10:12:03 +0100

    Add files via upload

---
+++ 1.4.c
@@ -0,0 +1,15 @@
+/* Prikazuje unos celog broja koristeci scanf("%d", &x) */
+#include <stdio.h>
+
+main()
+{
+	int x;
+	printf("Unesi ceo broj : ");
+
+	/* Obratiti paznju na znak & (operator uzimanja adrese)
+	   pre imena promenjive u funkciji scanf */
+	scanf("%d",&x);
+
+	/* U funkciji printf nije potrebno stavljati &  */
+	printf("Uneli ste broj %d\n", x);
+}
---
+++ 1.5.c
@@ -0,0 +1,11 @@
+/* Prikazuje zaukruzivanje realnog broja prilikom ispisa (%.2f) */
+#include <stdio.h>
+
+
+main()
+{
+	float x;
+	printf("Unesi realan broj : ");
+	scanf("%f",&x);
+	printf("Broj zaokruzen na dve decimale je : %.2f\n", x);
+}
